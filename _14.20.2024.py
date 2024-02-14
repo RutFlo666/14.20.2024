a6c5aaca8ebb2601be86b13a8a68dea6ac00c8f3
User <User@USER-PC39>
2024-02-14 16:56:42 +0200

    Добавьте файлы проекта.

---
+++ _14.20.2024.py
@@ -0,0 +1,23 @@
+#12
+# from random import randrange
+# arr = list()
+# for i in range(10):
+#     arr.append(randrange(1, 100))
+# max_i = arr.index(max(arr))
+# min_i = arr.index(min(arr))
+# arr[max_i] = min(arr)
+# arr[min_i] - min(arr)
+# for i in arr:
+#     print(i)
+    
+
+#11
+# abc = 'abcdefghijklmnopqrstuvwxyz'
+# ls = []
+# for i in range(0, len(abc)-1):
+#     ls.append(abc[i+1]*(i+1))
+# print(ls)
+
+#18
+# abc= '2,3,7,0,4'
+# print(abc[::-1]) 
